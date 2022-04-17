322f4e1bdb414f1f2ee279689f0a145d1f0d6669
lotem01 <lotem5035@gmail.com>
2022-04-17 14:09:04 +0300

    w players

--- chess.js
+++ chess.js
@@ -10,17 +10,116 @@ window.addEventListener('load', () => {
         let tr = document.createElement("tr");
         tb.appendChild(tr);
         for (let y=1; y<=8; y++)
-    {
+        {
             let td = document.createElement("td");
             tr.appendChild(td);
-            if ((y%2==1&& i%2==1) || (y%2==0&& i%2==0) ) {
+            if ((y%2==1&& i%2==1) || (y%2==0&& i%2==0) ) 
                 td.className= 'white';
-        }   else {
-            
-                td.className= 'black';
-            
-                
+                else 
+                    td.className= 'black';       
+                 
+            if (i==2)
+            {
+            let bPawn= new Image();
+            bPawn.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/800px-Chess_pdt45.svg.png';
+            bPawn.className='img1'
+            td.appendChild(bPawn);
+            }
+            if (i==7)
+            {
+            let wPawn= new Image();
+            wPawn.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/800px-Chess_plt45.svg.png';
+            wPawn.className='img1'
+            td.appendChild(wPawn);
+            }
+            if (i==1)
+            {
+                if ((y==1)||(y==8))
+                {
+                let bRook= new Image();
+                bRook.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/800px-Chess_rdt45.svg.png';
+                bRook.className='img1'
+                td.appendChild(bRook);
+                }
+
+                if ((y==2)||(y==7))
+                {
+                let bKnight= new Image();
+                bKnight.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/800px-Chess_ndt45.svg.png';
+                bKnight.className='img1'
+                td.appendChild(bKnight);
+                }
+
+                if ((y==3)||(y==6))
+                {
+                let bBishop= new Image();
+                bBishop.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/800px-Chess_bdt45.svg.png';
+                bBishop.className='img1'
+                td.appendChild(bBishop);
+                }
+
+                if (y==4)
+                {
+                let bQueen= new Image();
+                bQueen.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/800px-Chess_qdt45.svg.png';
+                bQueen.className='img1'
+                td.appendChild(bQueen);
+                }
+
+                if (y==5)
+                {
+                let bKing= new Image();
+                bKing.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/800px-Chess_kdt45.svg.png';
+                bKing.className='img1'
+                td.appendChild(bKing);
+                }
+            }
+            if (i==8)
+            {
+                if ((y==1)||(y==8))
+                {
+                let wRook= new Image();
+                wRook.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/800px-Chess_rlt45.svg.png';
+                wRook.className='img1'
+                td.appendChild(wRook);
+                }
+
+                if ((y==2)||(y==7))
+                {
+                let wKnight= new Image();
+                wKnight.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/800px-Chess_nlt45.svg.png';
+                wKnight.className='img1'
+                td.appendChild(wKnight);
+                }
+
+                if ((y==3)||(y==6))
+                {
+                let wBishop= new Image();
+                wBishop.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/800px-Chess_blt45.svg.png';
+                wBishop.className='img1'
+                td.appendChild(wBishop);
+                }
+
+                if (y==4)
+                {
+                let wQueen= new Image();
+                wQueen.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/800px-Chess_qlt45.svg.png';
+                wQueen.className='img1'
+                td.appendChild(wQueen);
+                }
+
+                if (y==5)
+                {
+                let wKing= new Image();
+                wKing.src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/800px-Chess_klt45.svg.png';
+                wKing.className='img1'
+                td.appendChild(wKing);
+                }
+            }
         }
     }
-    }
+    
+    
 });
+
+
